af46855a2b64c0fa201a16b14b8bf0c026457803
Kelly Keating <keating.kel@gmail.com>
2023-09-20 15:12:12 +1200

    AddRun title always shows

--- src/components/AddRun.tsx
+++ src/components/AddRun.tsx
@@ -24,7 +24,6 @@ function AddRun() {
     const distance = Math.abs(d)
     const time = Math.abs(t)
 
-    // TODO: check date is set to today or earlier
     if (points.find(point => point.distance === distance)) {
       alert('You already have a point for that distance')
     } else if (isNaN(distance) || isNaN(time)) {
@@ -39,38 +38,45 @@ function AddRun() {
   }
 
   const saveRun = () => {
+    // TODO: check date is set to today or earlier
+    // TODO: make sure I can't just push the button whenever
+    // aka make sure there's points? 
     if(date) createRun(dateToString(date), points)
     setDate(dayjs(new Date()))
     setPoints([])
     setShowForm(false)
   }
 
-  return showForm ? (
+  return (
     <div>
       <h2>Add a Run</h2>
-      <DatePicker value={date} maxDate={dayjs(new Date())} onChange={(newValue) => setDate(newValue)} />
-      <form onSubmit={addPoint}>
-        <label htmlFor="distance">Distance</label>
-        <input type="text" id="distance" />
-        <label htmlFor="time">Time</label>
-        <input type="text" id="time" />
-        {/* <TimePicker views={['minutes', 'seconds']} format="mm:ss" onChange={(val:any) => console.log(val)} /> */}
-        <button type="submit">Add</button>
-      </form>
-      {points && (
-        <>
-          <h3>Current points:</h3>
-          <ul>
-            {points.map((point: RunPoint) => (
-              <li key={point.distance}>{point.distance} km in {point.time} minutes <button onClick={() => removePoint(point)}>x</button></li>
-              ))}
-          </ul>
-        </>
+      {showForm ? (
+        <div>
+          <DatePicker value={date} maxDate={dayjs(new Date())} onChange={(newValue) => setDate(newValue)} />
+          <form onSubmit={addPoint}>
+            <label htmlFor="distance">Distance</label>
+            <input type="text" id="distance" />
+            <label htmlFor="time">Time</label>
+            <input type="text" id="time" />
+            {/* <TimePicker views={['minutes', 'seconds']} format="mm:ss" onChange={(val:any) => console.log(val)} /> */}
+            <button type="submit">Add</button>
+          </form>
+          {points && (
+            <>
+              <h3>Current points:</h3>
+              <ul>
+                {points.map((point: RunPoint) => (
+                  <li key={point.distance}>{point.distance} km in {point.time} minutes <button onClick={() => removePoint(point)}>x</button></li>
+                  ))}
+              </ul>
+            </>
+          )}
+          <button onClick={saveRun}>Save</button>
+        </div>
+      ) : (
+        <button onClick={() => setShowForm(true)}>Add Run</button>
       )}
-      <button onClick={saveRun}>Save</button>
     </div>
-  ) : (
-    <button onClick={() => setShowForm(true)}>Add Run</button>
   )
 }
 
