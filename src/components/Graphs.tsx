fdae03336d13e3309d85ed8068542288d3e59989
Kelly Keating <keating.kel@gmail.com>
2023-11-27 09:36:39 +1300

    try some charts

--- src/components/Graphs.tsx
+++ src/components/Graphs.tsx
@@ -1,13 +1,23 @@
 import { RunSet } from "../models"
+import { LineChart, Line } from 'recharts'
 
 interface Props {
   runs: RunSet
 }
 
 function Graphs({ runs }: Props) {
+
+  const data = Object.keys(runs).map(date => {
+    const run = runs[date]
+    return { date, speed: run.totalKms / run.totalMinutes }
+  })
+
   return (
     <div>
       <h2>Graphs</h2>
+      <LineChart width={400} height={400} data={data}>
+        <Line type="monotone" dataKey="speed" stroke="#8884d8" />
+      </LineChart>
     </div>
   )
 }
